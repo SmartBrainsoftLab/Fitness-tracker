13ab0287a92aa70e077050396c379585f7329690
Lam <lam.t.n.quang@gmail.com>
2024-11-10 18:38:54 -0800

    fix: app-structure-change

--- packages/react-package/src/App.tsx
+++ packages/react-package/src/App.tsx
@@ -1,53 +1,53 @@
-// import viteLogo from '/vite.png'
-// import './App.css'
-// import LogIn from './LogIn.tsx'
-// function App() {
-//   return (
-//     <>
-//       <img src={viteLogo} className="logo" alt="Vite logo" />
-//       <h3 className="h3-size">Welcome To</h3>
-//       <h2 className="h2-size">OUR FITNESS CARE</h2>
-//       <div className="card">
-//         <button className="btn-getStarted" onClick={LogIn}>
-//           Get Started
-//         </button>
-        
-//       </div>
-//     </>
-//   )
-// }
-
-// export default App
-
-
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { PATH } from './consts';
 
-import './App.css'
+// import './App.css'
 import StartPage from './pages/startpage/StartPage.tsx'
-import LogIn from './pages/signin/SignIn.tsx'
-import LogReg from './pages/signup/SignUp.tsx'
+import SignIn from './pages/signin/SignIn.tsx'
+import SignUp from './pages/signup/SignUp.tsx'
 import RegPage from './pages/regpage/RegPage.tsx'
 function App() {
   return (
-    <StrictMode>
+    // <StrictMode>
       <BrowserRouter>
         <Routes>
           <Route path={PATH.STARTPAGE} element={<StartPage />} />
-          <Route path={PATH.SIGNIN} element={<LogIn />} />
-          <Route path={PATH.SIGNUP} element={<LogReg />} />
+          <Route path={PATH.SIGNIN} element={<SignIn />} />
+          <Route path={PATH.SIGNUP} element={<SignUp />} />
           <Route path={PATH.REGPAGE} element={<RegPage />} />
         </Routes>
       </BrowserRouter>
-    </StrictMode>  
+    // </StrictMode>  
   )
 }
 
 export default App
 
+// import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+// import { PATH } from "./constant";
+// import { StartPage, SIGNIN, SIGNUP, RegPage,} from "pages";
+// import { PrivateRoute } from "components";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/ReactToastify.min.css";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//           <Route path={PATH.STARTPAGE} element={<StartPage />} />
+//           <Route path={PATH.SIGNIN} element={<SIGNIN />} />
+//           <Route path={PATH.SIGNUP} element={<SIGNUP />} />
+//           <Route path={PATH.REGPAGE} element={<RegPage />} />
+//       </Routes>
+//       <ToastContainer autoClose={1500} position="bottom-right" />
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
 
 
 
--- packages/react-package/src/main.tsx
+++ packages/react-package/src/main.tsx
@@ -1,47 +1,15 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// import { PATH } from './consts';
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+// import { Provider } from "react-redux";
+// import { store } from "store";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme.ts";
 
-// import './index.css'
-// import App from './App.tsx'
-// import LogIn from './LogIn.tsx'
-// import LogReg from './LogReg.tsx'
-// import RegPage from './RegPage.tsx'
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path={PATH.START} element={<App />} />
-//         <Route path={PATH.SIGNIN} element={<LogIn />} />
-//         <Route path={PATH.SIGNUP} element={<LogReg />} />
-//         <Route path={PATH.REGPAGE} element={<RegPage />} />
-//       </Routes>
-//     </BrowserRouter>
-//   </StrictMode>  
-// )
-
-console.log('index.ts');
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+createRoot(document.getElementById("root")!).render(
+  <ThemeProvider theme={theme}>
+    {/* <Provider store={store}> */}
+      <App />
+    {/* </Provider> */}
+  </ThemeProvider>
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-
-
